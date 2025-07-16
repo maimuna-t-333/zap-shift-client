5fb6c14eba67e859e938b10cdc6f541116ab4e25
maimuna-t-333 <maimuna.tabr@gmail.com>
2025-07-17 02:05:05 +0600

    payment method

--- src/Pages/DashBoard/Payment/PaymentForm.jsx
+++ src/Pages/DashBoard/Payment/PaymentForm.jsx
@@ -1,11 +1,12 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import React from 'react';
+import React, { useState } from 'react';
 
 const PaymentForm = () => {
     const stripe=useStripe();
     const elements=useElements()
+    const [error,setError]=useState('');
 
-    const handleSubmit=(e)=>{
+    const handleSubmit=async(e)=>{
         e.preventDefault();
 
         if(!stripe || !elements){
@@ -17,16 +18,33 @@ const PaymentForm = () => {
     if(!card){
         return
     }
+    const {error,paymentMethod}=await stripe.createPaymentMethod({
+         type:'card',
+         card
+    })
+    if(error){
+        setError(error.message)
+    }
+    else{
+        setError)''
+        console.log('payment Method',paymentMethod)
+    }
     }
     
     return (
         <div>
-            <form onSubmit={handleSubmit}>
-                <CardElement>
-                    <button type='submit' disabled={!stripe}>
+            <form onSubmit={handleSubmit} className='pace-y-4 bg-white p-6 rounded-xl shadow-md w-full max-w-md mx-auto'>
+                <CardElement className="p-2 border rounded">
+                    
+                </CardElement>
+                <button
+                     className="btn btn-primary text-black w-full"
+                    type='submit' disabled={!stripe}>
                         Pay for parcel
                     </button>
-                </CardElement>
+                    {
+                    error && <p className='text-red-500'>{error}</p>
+                }
             </form>
         </div>
     );
